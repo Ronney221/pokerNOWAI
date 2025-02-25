c6b2a9f8d3a57ed256d70f85b5d63093543beeb2
ronney221 <ronneydo1@gmail.com>
2025-02-24 21:05:51 -0800

    removed script

--- vite.config.js
+++ vite.config.js
@@ -6,38 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
-          'firebase-vendor': ['firebase/app', 'firebase/auth'],
-          'chart-vendor': ['react-chartjs-2', 'chartjs-plugin-annotation'],
-          'ui-vendor': ['react-toastify', '@heroicons/react', 'react-dropzone']
-        },
-        assetFileNames: (assetInfo) => {
-          const info = assetInfo.name.split('.');
-          const ext = info[info.length - 1];
-          if (/\.(png|jpe?g|svg|gif|tiff|bmp|ico)$/i.test(assetInfo.name)) {
-            return `assets/img/[name]-[hash][extname]`;
-          }
-          if (/\.css$/i.test(assetInfo.name)) {
-            return `assets/css/[name]-[hash][extname]`;
-          }
-          return `assets/[name]-[hash][extname]`;
-        },
-        chunkFileNames: 'assets/js/[name]-[hash].js',
-        entryFileNames: 'assets/js/[name]-[hash].js'
-      }
-    },
-    sourcemap: true,
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: false,
-        drop_debugger: true
-      }
-    }
+    sourcemap: true
   },
   server: {
     port: 5173,
@@ -47,8 +16,7 @@ export default defineConfig({
         changeOrigin: true
       }
     }
-  },
-  base: '/'
+  }
 })
 
 
