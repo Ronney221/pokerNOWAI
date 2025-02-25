53d8817387984e7632c88be2005445a449ee83df
ronney221 <ronneydo1@gmail.com>
2025-02-24 20:18:14 -0800

    rebuilt and cleared cache removed index.html buggy code

--- vite.config.js
+++ vite.config.js
@@ -1,52 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    emptyOutDir: true,
     assetsDir: 'assets',
-    sourcemap: false,
     rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html'),
-      },
       output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        },
-        assetFileNames: (assetInfo) => {
-          const info = assetInfo.name.split('.');
-          const ext = info[info.length - 1];
-          if (/\.(png|jpe?g|svg|gif|tiff|bmp|ico)$/i.test(assetInfo.name)) {
-            return `assets/img/[name]-[hash][extname]`;
-          }
-          if (/\.css$/i.test(assetInfo.name)) {
-            return `assets/css/[name]-[hash][extname]`;
-          }
-          return `assets/[name]-[hash][extname]`;
-        },
-        chunkFileNames: 'assets/js/[name]-[hash].js',
-        entryFileNames: 'assets/js/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js'
       }
     }
   },
   server: {
     port: 5173,
-    strictPort: true,
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
-        changeOrigin: true,
-      },
+        changeOrigin: true
+      }
     }
-  },
-  base: '/'
+  }
 })
 
 
