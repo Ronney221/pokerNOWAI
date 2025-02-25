b59a085ab6b52a47fdcb52d545dabfc062ea28fb
ronney221 <ronneydo1@gmail.com>
2025-02-24 20:30:36 -0800

    final try for tonight

--- vite.config.js
+++ vite.config.js
@@ -9,11 +9,30 @@ export default defineConfig({
     assetsDir: 'assets',
     rollupOptions: {
       output: {
-        assetFileNames: 'assets/[name]-[hash][extname]',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        entryFileNames: 'assets/[name]-[hash].js'
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+          'firebase-vendor': ['firebase/app', 'firebase/auth'],
+          'chart-vendor': ['react-chartjs-2', 'chartjs-plugin-annotation'],
+          'ui-vendor': ['react-toastify', '@heroicons/react', 'react-dropzone']
+        },
+        assetFileNames: (assetInfo) => {
+          const info = assetInfo.name.split('.');
+          const ext = info[info.length - 1];
+          if (/\.(png|jpe?g|svg|gif|tiff|bmp|ico)$/i.test(assetInfo.name)) {
+            return `assets/img/[name]-[hash][extname]`;
+          }
+          if (/\.css$/i.test(assetInfo.name)) {
+            return `assets/css/[name]-[hash][extname]`;
+          }
+          return `assets/[name]-[hash][extname]`;
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js'
       }
-    }
+    },
+    sourcemap: false,
+    // Add chunk size warnings
+    chunkSizeWarningLimit: 500
   },
   server: {
     port: 5173,
@@ -23,7 +42,8 @@ export default defineConfig({
         changeOrigin: true
       }
     }
-  }
+  },
+  base: '/'
 })
 
 
